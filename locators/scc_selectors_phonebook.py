b24eb18444e60b9ad7786419e24514776cadf503
Mariia Zakharevich <z0559882272@gmail.com>
2026-08-07 09:59:22 +0300

    added HW_css

--- locators/scc_selectors_phonebook.py
+++ locators/scc_selectors_phonebook.py
@@ -1,15 +1,48 @@
-#open browser
-#open site
+# open browser
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+
+try:
+    # open site
+    driver.get("https://telranedu.web.app/login")
+
+    # by tag_name
+    div = driver.find_element(By.TAG_NAME, "div")
+    div_1 = driver.find_element(By.CSS_SELECTOR, "div")
+
+    h1 = driver.find_element(By.TAG_NAME,"h1")
+    h1_1 = driver.find_element(By.CSS_SELECTOR, "h1")
+
+    input = driver.find_element(By.TAG_NAME, "input")
+    input_1 = driver.find_element(By.CSS_SELECTOR,"input")
+
+    a_list = driver.find_elements(By.TAG_NAME,"a")
+    a_list_1 = driver.find_elements(By.CSS_SELECTOR, "a")
+    print(len(a_list))
+
+# by class
+    container = driver.find_element(By.CLASS_NAME,"container")
+    container_1 = driver.find_element(By.CSS_SELECTOR,".container")
+
+    navbar = driver.find_element(By.CLASS_NAME,"navbar-component_nav__1X_4m")
+    navbar_1 = driver.find_element(By.CSS_SELECTOR, ".navbar-component_nav__1X_4m")
+
+    login_login = driver.find_element(By.CLASS_NAME,"login_login__3EHKB")
+    login_login_1 = driver.find_element(By.CSS_SELECTOR, ".login_login__3EHKB")
 
 
-#by tag_name
+# by id
+
+    root = driver.find_element(By.ID,"root")
+    root_1 = driver.find_element(By.CSS_SELECTOR,"#root")
+
+    root_2 = driver.find_elements(By.ID,"root")
+    root_3 = driver.find_elements(By.CSS_SELECTOR, "#root")
 
 
-#by class
 
-
-#by id
-
-
-
-#close browser
+# close browser
+finally:
+    driver.quit()
